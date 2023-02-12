79663203058e022eae4d70ab6a9bffce396e145c
Ugo (Pollyblo) <97290726+pollyblo@users.noreply.github.com>
2023-02-12 15:49:14 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/script/app.ts',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    watchFiles: ['./src/'],
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(s(a|c)ss)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
+    clean: true,
+  },
+};
